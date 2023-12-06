0a78b3496744584de3cf1029245f115a373ccd76
dinho <dinho@LAPTOP-D7O6TG3O>
2023-12-06 23:37:33 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using System.ComponentModel.Design;
+
+bool isMale = true;
+bool isTall = false;
+
+if (isMale && isTall)
+{
+    Console.WriteLine("You are a tall male");
+} else if (isMale && !isTall) {
+    Console.WriteLine("You are a short male");
+} else if (!isMale && isTall) {
+    Console.WriteLine("You are not a male but you are tall");
+} else
+{
+    Console.WriteLine("You are not male and not tall");
+}
+
+Console.ReadLine();
